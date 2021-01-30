fba3b0008a5b4d7a5fa5a12c8737c107ccd7ab0e
Mazen <63692059+mazenabdul@users.noreply.github.com>
2021-01-30 15:57:38 -0500

    Finished Challenge

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -6,16 +6,13 @@ const TaskContext = React.createContext()
 const reducer = (state, action) => {
   switch(action.type) {
     case 'add_task':
-      
       return { ...state,  taskStorage: [...state.taskStorage, action.payload] }
     case 'get_tasks':
-      //...JSON.parse(localStorage.getItem('tasks'))
       if(localStorage.getItem('tasks')){
         return { ...state, taskStorage: [ ...state.taskStorage, ...JSON.parse(localStorage.getItem('tasks')) ]}
       } else {
         return { ...state, taskStorage: [...state.taskStorage] }
-      }
-        
+      }  
     default: 
       return state
   }
@@ -32,20 +29,17 @@ export const TaskProvider = ({ children }) => {
  
   //Add a new task
   const addTask = (task) => {
-    
+  
     dispatch({ type: 'add_task', payload: task  })
     
-   
-    //console.log(localStorage.getItem('tasks'))
   }
 
   //Get Existing tasks
   const getTasks = () => {
-    //const data = JSON.parse(localStorage.getItem('tasks'))
+    
     dispatch({ type: 'get_tasks' })
   }
 
-  //Delete a task
 
 return <TaskContext.Provider value={{taskStorage: state.taskStorage, addTask, getTasks}}>{children}</TaskContext.Provider>
 }
